567287dac689e38b5cf72d02e96fdf93842b6e12
Firdevsssyildizzz <128640413+Firdevsssyildizzz@users.noreply.github.com>
2023-04-04 00:55:00 +0300

    Add files via upload

---
+++ EtsMobileApp/AnaSayfa/Model.swift
@@ -0,0 +1,11 @@
+
+import Foundation
+
+struct Veri: Codable{
+    let ad: String
+    let soyad: String
+    let dogumTarihi: String
+    let ePosta: String
+    let telefon: String
+    let not: String
+}
---
+++ EtsMobileAppUITests/EtsMobileAppUITests.swift
@@ -0,0 +1,32 @@
+
+import XCTest
+
+class EtsMobileAppUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+ 
+        continueAfterFailure = false
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+    func testExample() throws {
+
+        let app = XCUIApplication()
+        app.launch()
+
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
+
+//  Firdevs Yıldız tarafından 16.03.2023 tarihinde yapılmıştır.
